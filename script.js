1b0cf52a7849141ea588a5760d00bdcf5fc112b7
Polina Ryabova <polinaryabova.2@gmail.com>
2023-04-29 22:58:22 +0500

    add script.js with some bugs))

--- script.js
+++ script.js
@@ -1,72 +1,99 @@
-let outputStr = document.querySelector('.result').textContent;
-let bufValue = 0;
+const out = document.querySelector('.result');
+let fstNumb = '0'; //первое число
+let sndNumb = ''; // второе число
+let sign = '';
 
-const ChangeOutputStr = () => {
-    document.querySelector('.result').textContent = outputStr;
+const MathOp = {
+    Multiply: (a, b) => a * b,
+    Division: (a, b) => {
+        if (b === 0) return NaN;
+        else return a / b;
+    },
+    Sum: (a, b) => a + b,
+    Substract: (a, b) => a - b,
 };
 
-const Clear = () => {
-    bufValue = 0;
-    outputStr = '0';
-    ChangeOutputStr();
-};
+function ChangeOut(outputStr) {
+    out.textContent = outputStr;
+    if (sign === '') fstNumb = outputStr;
+    else sndNumb = outputStr;
+}
 
-const Backspace = () => {
-    if (outputStr.length === 1) {
+function Clear() {
+    fstNumb = '0';
+    sndNumb = '';
+    sign = '';
+    out.textContent = fstNumb;
+}
+
+function Backspace() {
+    let outputStr = out.textContent;
+    if (out.textContent.length === 1) {
         outputStr = '0';
     } else {
         outputStr = outputStr.slice(0, -1);
     }
 
-    ChangeOutputStr();
-};
+    ChangeOut(outputStr);
+}
 
-const Multiply = () => {};
-
-const Division = () => {};
-
-const Percent = () => {};
-
-const Plus = () => {
-    bufValue = Number(outputStr);
-    //outputStr = Number(outputStr) + bufValue;
-};
-
-const Minus = () => {
-    bufValue = Number(outputStr);
-    //outputStr = Number(outputStr) - bufValue;
-};
-
-const AddPoint = () => {
-    if (outputStr === '0') {
+function AddPoint() {
+    let outputStr = out.textContent;
+    if (outputStr === '0' || !Number(outputStr)) {
         outputStr = '0.';
     } else if (!outputStr.includes('.')) {
         outputStr += '.';
     }
 
-    ChangeOutputStr();
-};
+    ChangeOut(outputStr);
+}
 
-const AddNumber = (symbol) => {
-    if (outputStr === '0') {
+function AddNumber(symbol) {
+    let outputStr = out.textContent;
+    if (outputStr === '0' || (!Number(outputStr) && outputStr !== '0.')) {
         outputStr = symbol;
     } else {
         outputStr += symbol;
     }
 
-    ChangeOutputStr();
-};
+    ChangeOut(outputStr);
+}
+
+function Calculate(a, b, sign) {
+    switch (sign) {
+        case '+':
+            fstNumb = MathOp.Sum(a, b);
+            break;
+        case '−':
+            fstNumb = MathOp.Substract(a, b);
+            break;
+        case '÷':
+            fstNumb = MathOp.Division(a, b);
+            break;
+        case '×':
+            fstNumb = MathOp.Multiply(a, b);
+            break;
+    }
+    out.textContent = String(fstNumb);
+}
 
 const ChooseAction = (symbol) => {
     switch (symbol) {
         case '+':
-            Plus();
-            break;
         case '−':
-            Minus();
+        case '×':
+        case '÷':
+            out.textContent = symbol;
+            sign = symbol;
             break;
         case '=':
-            ChangeOutputStr();
+            if (fstNumb && sndNumb) {
+                Calculate(Number(fstNumb), Number(sndNumb), sign);
+            } else {
+                fstNumb = sndNumb;
+            }
+            ChangeOut(fstNumb);
+            sign = '';
             break;
         case 'C':
             Clear();
@@ -74,29 +101,22 @@ const ChooseAction = (symbol) => {
         case '':
             Backspace();
             break;
-        case '×':
-            Multiply();
-            break;
-        case '÷':
-            Division();
-            break;
-        case '%':
-            Percent();
-            break;
         case '.':
             AddPoint();
             break;
         default:
             AddNumber(symbol);
+            break;
     }
 };
 
 const init = () => {
     const btns = document.querySelectorAll('.calc__button');
     for (let btn of btns) {
-        btn.addEventListener('click', () =>
-            ChooseAction(btn.textContent.trim())
-        );
+        btn.addEventListener('click', () => {
+            window.navigator.vibrate(200);
+            ChooseAction(btn.textContent.trim());
+        });
     }
 };
 
